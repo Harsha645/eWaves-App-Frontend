1f9f66f1581ff3877582f5131660755cf4541628
Harsha645 <harshapramod19980124@gmail.com>
2023-05-24 01:46:58 +0530

    Topbar finished

--- src/component/topbar/Topbar.jsx
+++ src/component/topbar/Topbar.jsx
@@ -1,20 +1,44 @@
 import './topbar.css'
 import { AiOutlineSearch } from 'react-icons/ai'
+import { BsFillPersonFill, BsFillChatFill } from 'react-icons/bs'
+import { MdNotifications } from 'react-icons/md'
+
 
 
 const Topbar = () => {
   return (
-      <div className='topbarContainer'>
-        <div className="topbarLeft">
-          <span className="logo">eWaves App</span>
+    <div className='topbarContainer'>
+      <div className="topbarLeft">
+        <span className="logo">eWaves App</span>
+      </div>
+      <div className="topbarCenter">
+        <div className="searchbar">
+          <AiOutlineSearch className='searchIcon' />
+          <input placeholder='search for friend, post or video' className="searchInput" />
         </div>
-        <div className="topbarCenter">
-          <div className="serchbar">
-            <AiOutlineSearch/>
+      </div>
+      <div className="topbarRight">
+        <div className="topbarLinks">
+          <span className="topbarLinks">Homepage</span>
+          <span className="topbarLinks">Timeline</span>
+        </div>
+        <div className="topbarIcons">
+          <div className="topbarIconItem">
+            <BsFillPersonFill />
+            <span className="topbarIconBadge">1</span>
+          </div>
+          <div className="topbarIconItem">
+            <BsFillChatFill />
+            <span className="topbarIconBadge">3</span>
+          </div>
+          <div className="topbarIconItem">
+            <MdNotifications />
+            <span className="topbarIconBadge">7</span>
           </div>
         </div>
-        <div className="topbarRight"></div>
+        <img src="src/assets/tokio.PNG" alt="" className="topbarImg" />
       </div>
+    </div>
   )
 }
 
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Topbar from '../../component/topbar/Topbar'
+import sidebar from '../../src/component/sidebar/sidebar'
 
 const Home = () => {
   return (
     <div>
         <Topbar/>
+        <sidebar/>
     </div>
   )
 }
